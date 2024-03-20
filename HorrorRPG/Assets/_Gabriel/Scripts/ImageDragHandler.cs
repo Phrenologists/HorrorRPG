d17eb49e5f4cb6d7405ddf60ebe69f0f9b7d0793
Gabriel <61048404+XtraGabbi@users.noreply.github.com>
2024-03-20 21:43:58 +0100

    new script that allows for grouping of images on canvas by clicking and dragging over

---
+++ HorrorRPG/Assets/_Gabriel/Scripts/ImageDragHandler.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ImageDragHandler : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    RectTransform rectTransform;
+    Canvas canvas;
+    Vector2 originalPosition;
+    bool isDragging = false;
+    bool isHovering = false;
+    Image[] images;
+    List<Image> highlightedImages = new List<Image>();
+    List<Image> groupedImages = new List<Image>();
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        originalPosition = rectTransform.anchoredPosition;
+        FindImages();
+    }
+
+    void FindImages() => images = canvas.GetComponentsInChildren<Image>();
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+        Image draggedImage = gameObject.GetComponent<Image>();
+        if (draggedImage != null && !highlightedImages.Contains(draggedImage))
+            highlightedImages.Add(draggedImage);
+        HighlightImage();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isDragging)
+        {
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+            HighlightImage();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+        GroupImages();
+        UnhighlightImage();
+        highlightedImages.Clear();
+        groupedImages.Clear();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (isDragging)
+        {
+            isHovering = true;
+            HighlightImage();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (isDragging)
+        {
+            isHovering = false;
+            HighlightImage();
+        }
+    }
+
+    void HighlightImage()
+    {
+        foreach (Image image in images)
+        {
+            RectTransform otherRectTransform = image.GetComponent<RectTransform>();
+            if (isHovering && otherRectTransform != null && Vector2.Distance(rectTransform.anchoredPosition, otherRectTransform.anchoredPosition) < 50f)
+            {
+                if (!highlightedImages.Contains(image))
+                    highlightedImages.Add(image);
+            }
+            else if (image == gameObject.GetComponent<Image>())
+            {
+                image.color = Color.green;
+                if (!highlightedImages.Contains(image))
+                    highlightedImages.Add(image);
+            }
+            else if (highlightedImages.Contains(image))
+                image.color = Color.green;
+            else
+                image.color = Color.white;
+        }
+    }
+
+    void UnhighlightImage()
+    {
+        foreach (Image image in images)
+            image.color = Color.white;
+    }
+
+    void GroupImages()
+    {
+        List<string> groupedImageNames = new List<string>();
+        foreach (Image image in highlightedImages)
+        {
+            if (!groupedImages.Contains(image))
+                groupedImages.Add(image);
+        }
+        foreach (Image image in groupedImages)
+            groupedImageNames.Add(image.name);
+        if (groupedImageNames.Count > 0)
+            Debug.Log("Grouped Images: " + string.Join(", ", groupedImageNames.ToArray()));
+    }
+}
